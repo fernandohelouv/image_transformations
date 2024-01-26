e0795d59f47c931ca7be4a28c4b5e139dd6b7374
Fernando Hernández Loeza <fernando.h@soluciones-unificadas.mx>
2024-01-25 19:58:54 -0600

    Remove commented code and unused dictionary in train_shapes_classifier.py

--- train_shapes_classifier.py
+++ train_shapes_classifier.py
@@ -10,9 +10,3 @@ clasification_data = {}
 for image in images:
     # Get the data for the image
     r, dft_r = get_clasify_data(os.path.join(main_path, image))
-
-    # Print the data
-    # print(r, dft_r)
-
-    # Store the data in a dictionary, with the name of the image as the key
-    # clasification_data[image] = {"radious": r, "DFT_R": dft_r}
